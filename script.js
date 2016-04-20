57b1d26d45537c85a31c8bafca044929bd8ee2df
laurenegallium <laurene@gx42.mobi>
2016-04-20 12:58:34 -0700

    Update script.js
    changed replies

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about EstherBot? Just say HELLO to get started.')
+            return bot.say('So you want to learn about me? Just say HELLO.')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`I don't understand.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
