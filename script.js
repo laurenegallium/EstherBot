7a7b9878fbf584ff9051e2c0cef621a64bddd7fe
laurenegallium <laurene@gx42.mobi>
2016-04-21 13:46:54 -0700

    Update script.js
    changed replies

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I don't understand.`).then(() => 'speak');
+                    return bot.say(`Je ne comprende pa.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
